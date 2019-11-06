beb223411550bdf537a5c2d8fdb818112f5d3aec
zhangyongwang <20225614@qq.com>
2019-11-06 11:15:19 +0800

    更新

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ var server = http.createServer(function (request, response) {
     } else if (path === '/x') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
+        response.write(`h1{color: red;}`)
         response.end()
     } else {
         response.statusCode = 404
